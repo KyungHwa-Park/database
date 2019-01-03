2f93d066539fcef3eaf2c8cf7de7158ee8c96b80
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-03 18:25:14 +0900

    Smart_Baby project DB
    목록보기 및 상세보기 화면용 테이블

---
+++ record.sql
@@ -0,0 +1,41 @@
+drop table simple_list cascade constraints;
+
+create table record(
+    id              number                          primary key,
+    reg_date        date                            default(sysdate),
+    sleep_time      timestamp(0)                    not null,
+    wakeup_time     timestamp(0)                    not null,
+    total_time      interval day(3) to second(4),
+    day_night       varchar2(30)                    not null,
+    memo            clob,
+    update_date     date                            default(sysdate)
+);
+
+drop sequence       record_seq;
+create sequence     record_seq;
+
+
+
+
+drop table detail_list cascade constraints;
+
+create table detail_list(
+    id              number              primary key,
+    memo            varchar2(500),
+    reg_date        date                default(sysdate),
+    update_date     date                default(sysdate),
+    constraint simple_list foreign key(id) references simple_list(id)
+);
+
+insert into simple_list
+(id, reg_date, sleep_time, wakeup_time, day_night, update_date)
+values
+(1, sysdate, sysdate, sysdate, 'day', sysdate);
+
+update simple_list set total_time = (wakeup_time-sleep_time) day(3) to second(4);
+    
+select * from simple_list;
+
+select sleep_time, trunc(sleep_time, 'HH:MI')
+from simple_list;
+    
